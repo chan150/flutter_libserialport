6f28f7b52845b69bdc49a761332c3f303924d82f
Himchan Park <chan150@kaist.ac.kr>
2025-01-14 16:51:40 +0900

    ⚙ Upgrade AGP 8.4

--- android/src/main/kotlin/org/sigrok/flutter_libserialport/FlutterLibserialportPlugin.kt
+++ android/src/main/kotlin/org/sigrok/flutter_libserialport/FlutterLibserialportPlugin.kt
@@ -23,6 +23,7 @@ class FlutterLibserialportPlugin: FlutterPlugin, MethodCallHandler {
   }
 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+
     if (call.method == "getPlatformVersion") {
       result.success("Android ${android.os.Build.VERSION.RELEASE}")
     } else {
